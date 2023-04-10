9e347058bdd37f8caa5c8e748fcf980128fad74e
AntarcticPr <antarpramanik5@gmail.com>
2023-04-10 18:07:16 -0500

    added mysql into python file to see database

--- main.py
+++ main.py
@@ -1,4 +1,22 @@
 from tkinter import *
+import mysql.connector
+
+def connect_to_db():
+    return mysql.connector.connect(
+        host='localhost',
+        user='root',
+        password='Apple1!'
+    )
+
+if __name__ == '__main__':
+    connection = connect_to_db()
+    cursor = connection.cursor()
+    cursor.execute("SHOW DATABASES")
+    for x in cursor:
+        print(x)
+    cursor.close()
+    connection.close()
+
 root = Tk()
 
 myLabel = Label(root, text="Hello World! To the menu!")
